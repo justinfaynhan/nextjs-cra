f9a2aa08d7cb397223317da79ca46060527aff5a
Justin <justinnhan@hotmail.com>
2021-03-04 22:01:46 +1100

    Changes for next export functionality

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,39 +1,46 @@
-import axios from 'axios';
-import moment from 'moment';
-import { ThemeProvider } from 'mineral-ui/themes';
-import { PrimaryNav, NavItem } from 'mineral-ui/Navigation';
-import Table from 'mineral-ui/Table';
-import Text from 'mineral-ui/Text';
+// import axios from 'axios';
+import * as React from "react";
+import moment from "moment";
+import { ThemeProvider } from "mineral-ui/themes";
+import { PrimaryNav, NavItem } from "mineral-ui/Navigation";
+import Table from "mineral-ui/Table";
+import Text from "mineral-ui/Text";
 
-const Index = ({ users }) => (
-  <ThemeProvider>
-    <div>
-      <Text as="h1">Next.js</Text>
-      <PrimaryNav align="left" maxItemWidth="20rem">
-        <NavItem href="/">Create React App</NavItem>
-        <NavItem href="/nextjs" selected>Next.js</NavItem>
-      </PrimaryNav>
-      <Table
-         data={users}
-         rowKey="id"
-         title="Users"
-         hideTitle />
-     </div>
-   </ThemeProvider>
-);
+const Index = ({ hi }) => {
+  const [users, setUsers] = React.useState([]);
+  const getUsers = async () => {
+    const resp = await axios.get(`http://${req.headers.host}/api/data`);
+    setUsers(
+      resp.data.map((user) => {
+        return {
+          id: user.id,
+          FirstName: user.FirstName,
+          DateOfBirth: moment(user.DateOfBirth).format("MMMM Do YYYY"),
+        };
+      })
+    );
+  };
+  React.useEffect(() => {
+    getUsers();
+  }, []);
+  return (
+    <ThemeProvider>
+      <div>
+        <Text as="h1">Next.js</Text>
+        <PrimaryNav align="left" maxItemWidth="20rem">
+          <NavItem href="/">Create React App</NavItem>
+          <NavItem href="/nextjs" selected>
+            Next.js
+          </NavItem>
+        </PrimaryNav>
+        <Table data={users} rowKey="id" title="Users" hideTitle />
+      </div>
+    </ThemeProvider>
+  );
+};
 
-Index.getInitialProps = async({ req }) => {
-  console.time('nextjs:data');
-  const resp = await axios.get(`http://${req.headers.host}/api/data`);
-  console.timeEnd('nextjs:data');
-  const users = resp.data.map(user => {
-    return {
-      id: user.id,
-      FirstName: user.FirstName,
-      DateOfBirth: moment(user.DateOfBirth).format('MMMM Do YYYY'),
-    }
-  });
-  return { users };
-}
+Index.getStaticProps = async ({ req }) => {
+  return { hi: "hi" };
+};
 
 export default Index;
